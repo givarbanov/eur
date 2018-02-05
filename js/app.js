fddfa40e67fa474063d55c0113415ed094292648
Georgi Varbanov <georgi.varbanov@HAMILTONAPPS.LOCAL>
2018-02-05 17:46:33 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,31 +1,20 @@
 (function () {
 
-    // var elem = document.getElementsByClassName("invis");
-
     function isInViewport(element) {
         var rect = element.getBoundingClientRect();
-        // var html = document.documentElement;
-        
-        // console.log(element.className + " : top : " + rect.top);
-        // console.log("window.innerHeight: " + window.innerHeight);
-
         return (
             rect.top > 0 &&
-            rect.top + 150 < window.innerHeight //&&
-        //   rect.left >= 0 &&
-        //   rect.bottom <= (window.innerHeight || html.clientHeight) &&
-        //   rect.right <= (window.innerWidth || html.clientWidth)
+            rect.top + 150 < window.innerHeight 
         );
     }
 
     function eventHendler () {
-        // console.log(event);
-        var elem = document.getElementsByClassName("invis");
-        for(var i = 0; i < elem.length; i++) {
-            var el = elem.item(i);
+        var elems = document.getElementsByClassName("invis");
+        for(var i = 0; i < elems.length; i++) {
+            var el = elems.item(i);
             if (isInViewport(el)) {
                 if(el.classList.contains("scholarship__img")){
-                    el.classList.add("animimg");
+                    el.classList.add("anim-img");
                 } else {
                     el.classList.add("anim");
                 }
@@ -34,16 +23,19 @@
         };
     }
 
-    setInterval(eventHendler, 10);
-
-    // window.addEventListener("scroll", eventHendler, false);
-    // window.addEventListener("load", eventHendler, false);
-    // window.addEventListener('resize', eventHendler, false); 
-    
-    // document.addEventListener('click', function (event) {
-    //     if ( event.target.classList.contains( 'nav__link' ) ) {
-    //         setTimeout(eventHendler, 0);
+    // var nav = document.getElementsByClassName("nav")[0];
+    // nav.addEventListener("click", function (e) {
+    //     console.log(e);
+    //     e.preventDefault();
+    //     var source = e.srcElement;
+    //     if (source.classList.contains("nav__link")) {
+    //         document.querySelector(source.hash).scrollIntoView({
+    //             behavior: "smooth"
+    //         });
     //     }
-    // }, false);
+    //     e.stopPropagation();
+    // });
+
+    setInterval(eventHendler, 200);
 
 }());
